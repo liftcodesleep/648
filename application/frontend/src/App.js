3f5496378f40cc13dab4b7ea2ca371efd08094ee
nicburns <nburns@mail.sfsu.edu>
2023-02-17 17:27:48 -0800

    nic

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,7 +1,9 @@
 import About from './components/About'
 import Alekya from './components/Alekya'
 import Jacob from './components/Jacob'
-import Ishika from './components/Ishika'
+import Ishika from './components/Ishika'  
+import Nick from './components/Nick'
+
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -14,6 +16,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/alekya" element={<Alekya />} />
         <Route path="/jacob" element={<Jacob />} />
+        <Route path="/nick" element={<Nick />} />
           <Route path="/ishika" element={<Ishika />} />
       </Routes>
     </Router>
