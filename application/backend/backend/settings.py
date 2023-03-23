3a147d96e3635c00353b1aae31b4e7c13f29466f
ishika <ishikaishu2000@gmail.com>
2023-03-23 16:49:55 -0700

    CORS error fixed

--- application/backend/backend/settings.py
+++ application/backend/backend/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django', '127.0.0.1', "localhost",]
+ALLOWED_HOSTS = ['django', '127.0.0.1', 'http://localhost:3000']
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 
 # Application definition
