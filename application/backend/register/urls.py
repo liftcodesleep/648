f14fc08432912d9ac8b7ca1a3fa5a348990c8113
ishika <ishikaishu2000@gmail.com>
2023-03-24 16:03:33 -0700

    Removed extra module

--- application/backend/register/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.conf.urls import url
-from django.urls import path, include, re_path
-from .views import *
-
-app_name = 'register'
-
-urlpatterns = [
-    re_path('^$', render_register, name='register'),
-]
