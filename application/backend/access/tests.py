f997d2e6d8743b906fba9c8cc1f8a3c9645b1b1a
ishika <ishikaishu2000@gmail.com>
2023-04-21 15:05:39 -0700

    Added tests for login api

--- application/backend/access/tests.py
+++ application/backend/access/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class AccessTestCases(TestCase):
+    def test_successful_login(self):
+        input_payload = {
+            "username": "ishah_sfsu",
+            "password": "Ishika2510#",
+            "user_type": "general"
+        }
+
+        response = self.client.post('/user_login', input_payload, content_type='application/json')
+        self.assertEqual(response.json()['status'], 'SUCCESS')
+        self.assertEqual(response.json()['isLoggedin'], True)
+
+    def test_invalid_login(self):
+        input_payload = {
+            "username": "ishah_sfsu",
+            "password": "incorrect password",
+            "user_type": "general"
+        }
+
+        response = self.client.post('/user_login', input_payload, content_type='application/json')
+        self.assertEqual(response.json()['status'], 'SUCCESS')
+        self.assertEqual(response.json()['isLoggedin'], False)
