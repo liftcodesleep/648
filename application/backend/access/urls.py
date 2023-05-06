0234561cffc191128db02ae5ae3a89e2116018b4
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2023-05-06 14:55:01 -0700

    pulled development

--- application/backend/access/urls.py
+++ application/backend/access/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     re_path(r'^register_user$', register, name='register'),
     re_path(r'^login$', render_index_page, name='render_login'),
     re_path(r'^signup$', render_index_page, name='render_register'),
+    re_path(r'^$', render_index_page, name='render_homepage'),
     re_path(r'^logout$', logout, name='logout'),
     re_path(r'^user-profile$', render_index_page, name='user-profile'),
     re_path(r'^edit-profile$', render_index_page, name='edit-profile'),
